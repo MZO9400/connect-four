1c9d18b067ce32908851b11aa7e16d7aecac6d2c
Hamza <hamza.hameed00@gmail.com>
2024-07-13 13:54:44 -0400

    Transition color on win

--- src/component/Game.tsx
+++ src/component/Game.tsx
@@ -9,9 +9,11 @@ interface GameProps {
 const Game = ({firstPlayer}: GameProps) => {
   const {grid, turn, click, winner} = useConnectFour(firstPlayer)
 
+  const bgColor = winner === Color.NONE ? "bg-white" : 
+                  winner === Color.RED  ? "bg-red-300" : "bg-yellow-600"
+
   return (
-    <span className='h-screen flex flex-col justify-center items-center'>
-      {winner !== Color.NONE && <div>Winner is {winner}</div>}
+    <span className={`h-screen flex flex-col justify-center items-center transition-all duration-1000 ${bgColor}`}>
       <Grid grid={grid} turn={turn} click={click} />
     </span>
   )
